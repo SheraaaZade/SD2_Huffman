bb94f45b546201cba4e963f4b09c4cd434d7fcaa
20213136 <chehrazad.ouazzani@student.vinci.be>
2023-05-12 15:46:16 +0200

    priorityQueue

--- src/Huffman.java
+++ src/Huffman.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 
 public class Huffman {
@@ -21,11 +22,6 @@ public class Huffman {
         }
 
     }
-    public static void main(String[] args) {
-        String s="Bonjour! Au revoir!";
-        Map<Character, Integer> freq = computeFreq(s);
-        System.out.println(freq);
-    }
 
     // renvoie une map qui a comme clé les lettres de la chaine de
     // caractère donnée en paramètre et comme valeur la fréquence de
@@ -44,12 +40,35 @@ public class Huffman {
         return map;
     }
 
-    // renvoie l'arbre de Huffman obtenu à partir de la map des fréquences des lettres
-    public static Node buildTree(Map<Character, Integer> freq) {
-
-        return null;
+    public static void main(String[] args) {
+        String s="Bonjour! Au revoir!";
+        Map<Character, Integer> freq = computeFreq(s);
+        Node root = buildTree(freq);
+        System.out.println("Nbre de lettre dans la chaine de caractère à encoder: "
+                + root.freq );
+        System.out.println("Fréquence des lettres dans le sous-arbre de gauche: "
+                + root.left.freq );
+        System.out.println("Fréquence des lettres dans le sous-arbre de droite: "
+                + root.right.freq );
     }
 
+    // renvoie l'arbre de Huffman obtenu à partir de la map des fréquences des lettres
+    public static Node buildTree(Map<Character, Integer> freq) {
+        PriorityQueue priorityQueue = new PriorityQueue<>();
+
+        for (Character character : freq.keySet()) {
+            Node node = new Node(character, freq.get(character), null ,null);
+            priorityQueue.add(node);
+        }
+        while(priorityQueue.size() > 1){
+            Node node = (Node) priorityQueue.poll();
+        }
+        return null;
+    }
+    //EXAMEN
+    //soit bfs et l'afficher en matrice ou liste d'adjacence
+    // ou le bfs déjà fait mais savoir retrouver lee chemin inverse le sommet précédent
+    // soit papier soit implémentation pour MinMax
     // renvoie une map qui associe chaque lettre à son code (suite de 0 et de 1).
     // Ce code est obtenu en parcourant l'arbre de Huffman donné en paramètre
     public static Map<Character, String> buildCode(Node root) {
